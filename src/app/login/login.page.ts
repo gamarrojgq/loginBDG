c75520c3a0af1dc5ceba5bfe6b4ed50077ad688c
Josue <gamarrojgq@gmail.com>
2019-07-09 09:00:46 -0600

    feat: Login y Register

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../entity/user.class';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  user: User = new User();
+  constructor(private authService: AuthService, private router: Router,
+    private toastController: ToastController) { 
+      this.user.email = 'jgamarro@bdgsa.net';
+      this.user.password = '123456';
+    }
 
   ngOnInit() {
+  }
+
+  async onLogin(){
+    const resultado: any = await this.authService.onLogin(this.user);
+    if (resultado.code)
+    {
+      return this.presentToast(resultado.message);
+    }
+    this.presentToast('Bienvenido');
+    this.router.navigateByUrl('/');
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000
+    })
+    toast.present();
   }
 
 }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../entity/user.class';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -8,12 +11,31 @@ import { User } from '../entity/user.class';
 })
 export class RegisterPage implements OnInit {
   user: User = new User();
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router, 
+    private toastController: ToastController ) { }
 
   ngOnInit() {
   }
 
-  onRegister(){
-
+  async onRegister(){
+    const resultado: any = await this.authService.onRegister(this.user);
+    console.log(resultado);
+    if(resultado.code){
+      this.presentToast(resultado.message);
+      return console.log(resultado.message);
+    }
+    this.router.navigateByUrl('/');
   }
+
+  presentToast(mensaje: string) {
+    this.toastController.create({
+      message: mensaje,
+      duration: 2000
+    })
+      .then(res => {
+        console.log('Promesa');
+        res.present();
+    });
+  }
+
 }
